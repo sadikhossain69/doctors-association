75863663cfebd10d9fbb73611dc2cc22d247c116
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-10 00:06:15 +0600

    navbar ready

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import PageNotFound from './Pages/PageNotFound/PageNotFound';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='*' element={<PageNotFound/>} />
         </Routes>
       </div>
     </>
---
+++ src/Pages/PageNotFound/PageNotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div className='h-screen flex items-center justify-center' >
+            <div className='text-center'>
+                <h2 className='text-3xl'>404</h2>
+                <h2 className='text-3xl'>Page Not Found!!!</h2>
+            </div>
+        </div>
+    );
+};
+
+export default PageNotFound;
