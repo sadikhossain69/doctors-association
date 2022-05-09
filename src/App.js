df59e3a2e4624c35b6620e19f7cd7a7570ec7626
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-09 23:15:23 +0600

    photo added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div className="text-3xl font-bold underline text-center text-red-600">
+        Hello world!
+      </div>
+      <div className='text-center'>
+        <button class="btn btn-primary">Button</button>
+      </div>
+    </>
   );
 }
 
