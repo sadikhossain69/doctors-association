92b4e84b0e5380a97b3552abb7daf8c1b70aa256
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-10 19:23:44 +0600

    treatment section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import Info from './Info'
 import Services from './Services';
+import Treatment from './Treatment';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <Info/>
             <Services/>
+            <Treatment/>
         </div>
     );
 };
---
+++ src/Pages/Home/Treatment.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import treatment from '../../assets/images/treatment.png'
+
+const Treatment = () => {
+    return (
+        <div className="hero min-h-screen mx-auto">
+            <div className="hero-content flex-col lg:flex-row gap-16">
+                <img src={treatment} className="sm:w-80 rounded-lg shadow-2xl" alt='' />
+                <div>
+                    <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms!</h1>
+                    <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                    <button className="btn btn-primary bg-gradient-to-l from-cyan-500 to-blue-500 text-white">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Treatment;
