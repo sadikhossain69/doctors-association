b0e00fc1362abb5f839b319e4120cdd2c5a1e2d4
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-11 15:19:59 +0600

    some initial upgrade

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import PageNotFound from './Pages/PageNotFound/PageNotFound';
-import Footer from './Pages/Shared/Footer/Footer';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      <div>
+      <div className=' max-w-7xl mx-auto'>
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>} />
@@ -18,7 +17,6 @@ function App() {
           <Route path='/login' element={<Login/>} />
           <Route path='*' element={<PageNotFound/>} />
         </Routes>
-        <Footer/>
       </div>
     </>
   );
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const infoCard = ({ img, cardTitle, bgClass }) => {
     return (
         <div className={`card lg:card-side shadow-xl ${bgClass}`}>
-            <figure className='pl-5'>
+            <figure className='pl-5 p-5'>
                 <img src={img} alt="Album" />
             </figure>
             <div className="card-body text-white">
