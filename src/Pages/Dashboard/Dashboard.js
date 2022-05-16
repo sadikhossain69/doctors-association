eebb13d0556577426f92320f91146d3304130b55
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-17 02:22:52 +0600

    now only admin can see the all userse

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
+
+    const [user] = useAuthState(auth)
+
+    const [admin] = useAdmin(user)
+
     return (
         <div className="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
@@ -16,7 +24,7 @@ const Dashboard = () => {
                 <ul className="menu p-4 overflow-y-auto w-52 bg-base-100 text-base-content">
                     <li><Link to='/dashboard'>My Appointment</Link></li>
                     <li><Link to='/dashboard/review'>My Review</Link></li>
-                    <li><Link to='/dashboard/users'>All Users</Link></li>
+                    { admin && <li><Link to='/dashboard/users'>All Users</Link></li>}
                 </ul>
 
             </div>
--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -19,7 +19,7 @@ const MyAppointments = () => {
                 }
             })
                 .then(res => {
-                    console.log("res", res);
+                    // console.log("res", res);
                     if(res.status === 401 || res.status === 403) {
                         signOut(auth)
                         localStorage.removeItem('accessToken')
